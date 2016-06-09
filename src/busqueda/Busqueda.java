180837f6b84a0cfe251d241904f8e677db79ff58
Pablite5 <Pablite5@GAMING>
2016-06-09 09:39:19 +0200

    commit consola, Visualizamos todo por consola.

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -1,15 +1,23 @@
 package busqueda;
 
+import java.util.Scanner;
+
 public class Busqueda {
 
     public static void main(String[] args) {
-        int[] numeros = new int[100];
+        System.out.println("numero del Array:");
+        Scanner sc1 = new Scanner(System.in);
+        int numArray=sc1.nextInt();
+        System.out.println("número que quieras ");
+        Scanner sc= new Scanner(System.in);
+        int numero=sc.nextInt();
+         int[] numeros = new int[numArray];
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = i * 5;
         }
         Busqueda ejemplo = new Busqueda();
         int indice = ejemplo.busquedaBinaria(numeros, 450, 0, numeros.length - 1);
-        System.out.println("El indice del valor '450' es: " + indice);
+        System.out.println("El indice del valor " +numArray + " es" + indice);
     }
 
     public int busquedaBinaria(int[] listado, int clave, int posInicial, int posFinal) {
